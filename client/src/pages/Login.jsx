068efbc65a02791dc5775cb93370371e3ad7278a
vikash874u <167596087+vikash874u@users.noreply.github.com>
2025-09-13 18:11:07 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const res = await axios.post("https://exam-prep-3ee5.onrender.com/api/examinee/login", form);
+    const res = await axios.post("https://exam-prep-3ee5.onrender.com/api/admin", form);
 
     if (res.data.message === "Login Successfully") {
       alert("Login successfully");
