1a7e115092037c5d894895f270fef44ba29d0139
vikash874u <167596087+vikash874u@users.noreply.github.com>
2025-09-13 16:50:22 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const dotenv = require('dotenv');
 const app = express();
+dotenv.config();
 app.use(cors())
 app.use(express.json());
 
 //Mongodb connection
-const URL ='mongodb://localhost:27017/examprep'
+const URL = process.env.MONGUrl || 'mongodb://localhost:27017/examprep'
 mongoose.connect(URL)
 .then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB connection error:', err));
@@ -23,6 +24,6 @@ app.use("/api/examinee/",require('./routes/examineeRoute'))
 app.use("/api/message/",require('./routes/MessageRoute'))
 // api end
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server is running on http://localhost:5000/")
 })
